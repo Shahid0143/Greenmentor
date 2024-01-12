99bded8af168d7c4258d84611ef158e723024b0d
Shahid Raza <aizansami123@gmail.com>
2024-01-11 17:36:15 -1200

    done

---
+++ Green-project/Green_project/src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+
+import Datalist from './components/Datalist'
+
+// import Siderbar from './components/Siderbar'
+
+function App() {
+ 
+  
+
+  return (
+    <>
+      <div>
+        {/* <Siderbar /> */}
+        <Datalist />
+        
+      </div>
+    </>
+  );
+}
+
+export default App
